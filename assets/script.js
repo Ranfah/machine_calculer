f332d71c7a3efc8f339f24916d41dc9f0cba74bd
faniry ran <faniryran01@gmail.com>
2023-07-20 14:13:02 +0300

    machine calculer

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+const display = document.querySelector(".display")
+const button = document.querySelectorAll("button")
+
+const specialCharter = ["%","*","/","-","+","="]
+let output = ""
+
+const calculate = (btnValue) =>{
+    display.focus();
+    if (btnValue==="=" && output !==""){
+        output = eval(output.replace("%","/100"))
+    }else if(btnValue === "AC"){
+        output=""
+    }else if(btnValue==="DEL"){
+        output = output.toString().slice(0.-1)
+    }else{
+        if(output === "" && specialCharter.includes(btnValue)) return;
+        output += btnValue
+    }
+    display.value = output
+}
+button.forEach((button)=>{
+    button.addEventListener("click",(e)=>{
+        calculate(e.target.dataset.value)
+    })
+})
